0b1cf93e9e8fe1de2de26d0cc6d8967db8c675d2
pgmoody <peter.g.moody@gmail.com>
2017-04-29 13:25:38 -0400

    initial commit

---
+++ iondie/DnsEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iondie
+{
+    public class DnsEntry
+    {
+        public string Name { get; set; }
+        public DNS_RECORD_TYPE Type { get; set; }
+
+        public DnsEntry()
+        {
+            this.Name = String.Empty;
+            this.Type = DNS_RECORD_TYPE.DNS_TYPE_ZERO;
+        }
+
+        public DnsEntry(string name, uint recordType)
+        {
+            this.Name = name;
+            this.Type = (DNS_RECORD_TYPE)recordType;
+        }
+
+        public DnsEntry(string name, DNS_RECORD_TYPE recordType)
+        {
+            this.Name = name;
+            this.Type = recordType;
+        }
+    }
+}
---
+++ iondie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace iondie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = DnsInfo.GetDnsCacheEntries();
+
+            Console.WriteLine(results.Count);
+
+            foreach (var entry in results)
+            {
+                Console.WriteLine(entry.Name + "\t" + Enum.GetName(typeof(DNS_RECORD_TYPE), entry.Type));
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
